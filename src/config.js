760cc4be6a954dfed80c4c882a7ab7e2f8f4a476
Kyle Forkey <kyleforkey@ethmint.io>
2021-10-14 15:37:54 -0500

    yes

--- src/config.js
+++ src/config.js
@@ -6,19 +6,19 @@ const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
   "Nifty Narwhal is a community-run creative studio that empowers creative enthusiasts to influence the art with which we engage. 10,000 unique Ethereum-based tokens will function as membership passes, granting access to creative contributions and joint ownership of our group's works. The Nifty Narwhals Creative Studio was formed to collaborate on media projects with hundreds of participants and to give ownership of our collective works.s";
-const baseUri = "ipfs://QmUGhRbtuQQijbL8tUE4uY5MZQA3qXyL6DE445LhazCyKj";
+const baseUri = "ipfs://QmSouXRmu4KfnaZvBddMt38dvp1gR1cBTwukfad3Rht66w";
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 50,
+    growEditionSizeTo: 8000,
     layersOrder: [
       { name: "Background" },
       { name: "Body" },
+      { name: "Mouth" },
       { name: "Clothes" },
       { name: "Eyes" },
       { name: "Eyewear" },
       { name: "Hats" },
-      { name: "Mouth" },
       { name: "Tusks" },
     ],
   },
